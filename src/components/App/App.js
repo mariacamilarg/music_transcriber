a6ce707b1563dfaf9346a8a827fe6f0be0b7f6ce
Maria Camila Remolina-Gutiérrez <3608776+mariacamilarg@users.noreply.github.com>
2020-09-30 18:40:52 -0500

    Adding stave to master (#2)
    * stave
    
    * stave2
    
    * painting stave
    
    * display notes
    
    * Stave class
    
    * Stave Notes
    
    * initial commit removing double div
    
    * putting notes and button outside mystaff class
    
    * translating function to class, still not working
    
    * moving vexflow config to componendimount
    
    * it works now :D
    
    * src/
    
    * temp
    
    * now is painting double again, ready to remove duplicates
    
    * cleaining it up, it is still painting it on top, need to clean the component
    
    * clearing group now deletes previous canvas
    
    Co-authored-by: Edwige Gros <edwige.gros@u-psud.fr>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,59 @@
 import React from 'react';
-//import logo from '../../images/logo.svg';
+import Piano from '../Piano/Piano';
+import Staff from '../Staff/Staff';
+//import StaffNote from '../Staff/StaffNote';
+
 import './App.css';
 
-import Piano from '../Piano/Piano';
+class App extends React.Component {
 
-function App() {
-
-  var state = {
-    msg: "this should update the stave",
-    operation: null,
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          keys: ["c/4"],
+          duration: "1",
+        },
+        {
+          keys: ["d/4"],
+          duration: "1",
+        },
+        {
+          keys: ["e/4"],
+          duration: "1",
+        }        
+        // new StaffNote(["c/4"], "1"),
+        // new StaffNote(["d/4"], "1"),
+        // new StaffNote(["e/4"], "1"),
+      ],  
+    };
   };
 
-  function handleClick(note) {
-    //this.setState(calculate(this.state, buttonName));
+  handleClick(note) {
     console.log(note);
-    console.log(state.msg);
+    console.log("this should update the stave");
   };
 
-  return (
-    <div className="App">
-      <h1>Music transcriber</h1>
-      <br />
-      <Piano clickHandler={handleClick} />
-    </div>
-  );
+  addNote(note) {
+    console.log("New note : " + note.keys);
+    this.setState({
+      notes: this.state.notes.concat([note])
+    });
+    //this.forceUpdate();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Music transcriber</h1>
+        <Piano clickHandler={this.handleClick} />
+        <br />
+        <button onClick={() => this.addNote({keys: ["b/4"], duration: "1"})}> Add B/4 </button>
+        <Staff clef='treble' timeSignature='4/4' notes={this.state.notes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Staff/Staff.js
@@ -0,0 +1,150 @@
+import React from 'react'
+import PropTypes from "prop-types";
+import VexFlow from 'vexflow'
+import './Staff.css';
+
+class Staff extends React.Component {
+
+    static propTypes = {
+        clef: PropTypes.string,
+        timeSignature: PropTypes.string,
+        notes: PropTypes.array,
+    };
+
+    constructor(props){
+        super(props);
+
+        this.VF = VexFlow.Flow;
+        this.width = 600;
+        this.height = 150;
+
+        // Refs for the div container and the renderer
+        this.container = React.createRef();
+        this.rendererRef = React.createRef();
+    }
+
+    parseNotes (unparsedNotes) {
+
+        // VF
+        const { StaveNote } = this.VF;
+
+        return unparsedNotes
+            .map(
+                ({ keys, duration = '1' }) =>
+                    new StaveNote({
+                        clef: this.props.clef,
+                        keys: keys,
+                        duration: duration,
+                    })
+            );
+
+        // return unparsedNotes
+        //     .map(
+        //         note => (typeof note === 'string' ? { key: note } : note)
+        //     )
+        //     .map(
+        //         note => Array.isArray(note) ? { key: note[0], duration: note[1] } : note
+        //     )
+        //     .map(
+        //         ({ key, ...rest }) =>
+        //             typeof key === 'string'
+        //             ? { key: key.includes('/') ? key : `${key[0]}/${key.slice(1)}`, ...rest, }
+        //             : rest
+        //     )
+        //     .map(
+        //         ({ key, keys, duration = '1' }) =>
+        //             new StaveNote({
+        //                 keys: key ? [key] : keys,
+        //                 duration: String(duration),
+        //             })
+        //     );
+    }
+
+    initializeRenderer() {
+        // VF
+        const { Renderer } = this.VF;
+
+        // Renderer
+        if (this.rendererRef.current == null) {
+            this.rendererRef.current = new Renderer(this.container.current, Renderer.Backends.SVG);
+        }
+
+        this.renderer = this.rendererRef.current;
+        this.renderer.resize(this.width, this.height);
+    }
+
+    initializeContext() {
+        // Context
+        this.context = this.renderer.getContext()
+        this.context.setFont('Arial', 10, '').setBackgroundFillStyle('#eed')
+    }
+
+    paintStaff () {
+        
+        // VF
+        const { Stave } = this.VF;
+
+        // Stave
+        this.stave = new Stave(0, 0, this.width);
+        this.stave.addClef(this.props.clef).addTimeSignature(this.props.timeSignature);
+        this.stave.setContext(this.context).draw()
+    }
+
+    paintNotes () {
+
+        // VF
+        const { Formatter } = this.VF;
+
+        // Notes
+        this.parsedNotes = this.parseNotes(this.props.notes);
+
+        // Update context
+        this.context = this.renderer.getContext()
+        
+        // Formatter
+        Formatter.FormatAndDraw(this.context, this.stave, this.parsedNotes, {
+           auto_beam: true,
+        });
+    }
+
+    paint () {
+        this.initializeRenderer();
+        this.initializeContext();
+
+        // Open Group
+        this.group = this.context.openGroup();
+
+        this.paintStaff();
+        this.paintNotes();
+
+        // Close group:
+        this.context.closeGroup();
+    }
+
+    clear () {
+        // Delete the existing group to paint again
+        this.context = this.renderer.getContext();
+        this.context.svg.removeChild(this.group);
+    }
+
+    componentDidMount () {
+        console.log("component mounted " + this.props.notes.map(note => note.keys));
+        
+        this.paint();
+    }
+
+    componentDidUpdate () {
+        console.log("component updated " + this.props.notes.map(note => note.keys));
+
+        this.clear();
+        this.paint();
+    }
+
+    render(){
+        return (
+            <div id='Stave' ref={this.container} />
+        );
+    }
+}
+
+export default Staff;
