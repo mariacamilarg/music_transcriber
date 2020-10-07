09baeaf5776f224bb42a47db599caa83bf23fa0e
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2020-10-07 10:35:56 -0500

    now it is possible to enter own youtube url

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -5,6 +5,9 @@ import './Video.css';
 
 /*
 * https://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0200332
+* https://github.com/CookPete/react-player
+* https://github.com/CookPete/react-player/blob/master/src/demo/App.js
+* https://cookpete.com/react-player/
 */
 
 class Video extends React.Component {
@@ -13,10 +16,40 @@ class Video extends React.Component {
     url: PropTypes.string,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      enteredUrl: this.props.url,
+      videoUrl: this.props.url,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  handleChange(event) {
+    this.setState({
+      enteredUrl: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      videoUrl: this.state.enteredUrl
+    });
+  }
+
   render() {
     return (
       <div className='component-video'>
-        <ReactPlayer url={this.props.url} width='100%' height='100%'/>
+        <form className='search-video' onSubmit={this.handleSubmit}>
+          <label>
+            <input className='search-bar' type="text" value={this.state.enteredUrl} onChange={this.handleChange} />
+          </label>
+          <input className='search-button' type="submit" value="OK" />
+        </form>
+        <ReactPlayer url={this.state.videoUrl} width='100%' height='85%'/>
       </div>
     );
   }
