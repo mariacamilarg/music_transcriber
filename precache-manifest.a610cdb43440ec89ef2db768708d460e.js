6588e1ac29c5bbdcc243f39227bece36cbd3c58c
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2020-10-20 04:17:22 -0500

    Updates

---
+++ precache-manifest.a610cdb43440ec89ef2db768708d460e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7944cbee533ac87abdd668aaa674fecb",
+    "url": "/music_transcriber/index.html"
+  },
+  {
+    "revision": "c88db9e8502a13ae2e8f",
+    "url": "/music_transcriber/static/css/main.e8fc488f.chunk.css"
+  },
+  {
+    "revision": "72f03f60babe95d2d98c",
+    "url": "/music_transcriber/static/js/2.8ebd653b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/music_transcriber/static/js/2.8ebd653b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88db9e8502a13ae2e8f",
+    "url": "/music_transcriber/static/js/main.e74dcb23.chunk.js"
+  },
+  {
+    "revision": "509b592d079f921d6fd4",
+    "url": "/music_transcriber/static/js/runtime-main.58378366.js"
+  }
+]);
